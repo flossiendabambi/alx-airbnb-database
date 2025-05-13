0ede40401d8b23b5e9a0755935bf347346ed51a7
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-13 17:18:54 +0800

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,9 +2,10 @@ SELECT Booking.booking_id, User.first_name, User.last_name
   FROM Booking
 INNER JOIN User ON Booking.user_id = User.user_id;
 
-SELECT Property.property_id, Property.name, Review.review_id, Review.rating
+SELECT Property.name, Review.rating
   FROM Property
-  LEFT JOIN Review ON Property.property_id = Review.property_id;
+  LEFT JOIN Review ON Property.property_id = Review.property_id
+  ORDER BY Review.rating DESC;
 
 SELECT User.first_name, User.last_name, Booking.booking_id
 FROM Booking
