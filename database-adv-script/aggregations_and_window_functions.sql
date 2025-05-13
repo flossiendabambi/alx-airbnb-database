651c7fed041a95489e9d6a8f4bbebfdd8a8321c8
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-14 01:28:27 +0800

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,3 +2,16 @@ SELECT User.first_name, User.last_name, COUNT(Booking.booking_id) AS total_booki
 FROM Booking
 JOIN User ON Booking.user_id = User.user_id
 GROUP BY User.first_name, User.last_name;
+
+
+SELECT 
+  name,
+  total_bookings,
+  ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_num,
+  RANK() OVER (ORDER BY total_bookings DESC) AS rank_num
+FROM (
+  SELECT Property.name, COUNT(Booking.booking_id) AS total_bookings
+    FROM Booking
+    JOIN Property ON Booking.user_id = Property.user_id
+    GROUP BY Property.name
+  AS BookingTable;
