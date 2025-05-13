c2078e47ba8e564c35804624ab53aae6db339ea2
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-13 23:21:38 +0800

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,4 @@
-
+SELECT User.first_name, User.last_name, COUNT(Booking.booking_id) AS total_bookings
+FROM Booking
+JOIN User ON Booking.user_id = User.user_id
+GROUP BY User.first_name, User.last_name;
