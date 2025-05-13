d2c721a0e9e1bd9db028f82614c160f4e63957d1
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-13 17:04:37 +0800

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,9 @@
 SELECT Booking.booking_id, User.first_name, User.last_name 
   FROM Booking
 INNER JOIN User ON Booking.user_id = User.user_id;
+
+
+
+SELECT User.first_name, User.last_name, Booking.booking_id
+FROM Booking
+FULL OUTER JOIN User ON Booking.user_id = User.user_id;
