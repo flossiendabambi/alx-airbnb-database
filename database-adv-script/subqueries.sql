d00f2d13b419f73c7119aae2d6746d2f56e1b7a8
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-13 17:55:45 +0800

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -5,3 +5,11 @@ WHERE property_id IN (
   FROM Review
   WHERE AVG(rating) > 4.0
   );
+
+SELECT DISTINCT User.user_id, User.first_name, User.last_name
+FROM User
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking b
+    WHERE b.user_id = User.user_id
+) > 3;
