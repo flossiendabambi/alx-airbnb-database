b6a90b1ed5dc365e1081780c9fa0ee5a20689b21
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-15 16:47:35 +0800

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -21,7 +21,9 @@ SELECT
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+    WHERE b.status = 'confirmed'
+    AND u.first_name = 'Alice';
 
 
 
